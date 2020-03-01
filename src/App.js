1495814470af33e1e6db9b5339e4782badb9dfc7
bergmaner <bergkacper@gmail.com>
2020-03-01 15:04:41 +0100

    collide head to Snake

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   componentDidUpdate()
   {
     this.checkSnakeIsOutOfBorders();
+    this.checkHeadIsOnSnake();
   }
   onKeyDown = (e) =>
   {
@@ -76,6 +77,16 @@ class App extends Component {
     let head = this.state.snakeSquares[this.state.snakeSquares.length-1];
     if(head[0]<0 || head[0]>=96 || head[1]<0 || head[1]>=96)//check is it out of order
     this.gameOver();
+  }
+  checkHeadIsOnSnake()
+  {
+    let snake = [...this.state.snakeSquares];
+    let head = snake[snake.length-1];
+    snake.pop();//remove head position from snake table
+    snake.forEach(square =>{
+      if(head[0]===square[0] && head[1] === square[1])//check head position with snake table
+      this.gameOver();
+    })
   }
   gameOver()
   {
