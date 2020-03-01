eb0b99539b4be89044bb2fbeb1cda08580032d3f
bergmaner <bergkacper@gmail.com>
2020-03-01 14:56:25 +0100

    move Snake

--- src/App.js
+++ src/App.js
@@ -19,8 +19,54 @@ const initialState =
 class App extends Component {
  
   state = initialState;
- 
- 
+  componentDidMount()
+  {
+    setInterval(this.moveSnake,this.state.speed);
+    document.onkeydown = this.onKeyDown;
+  }
+  onKeyDown = (e) =>
+  {
+    e = e || window.event;
+    switch(e.keyCode)
+    {
+      case 38 : 
+      this.setState({direction:'UP'});
+      break;
+      case 40 : 
+      this.setState({direction:'DOWN'});
+      break;
+      case 37 : 
+      this.setState({direction:'LEFT'});
+      break;
+      case 39 : 
+      this.setState({direction:'RIGHT'});
+      break;
+    }
+  }
+  moveSnake = () =>
+  {
+    let squares = [...this.state.snakeSquares];
+    let head = squares[squares.length-1];
+    let food = this.state.food;
+    // 4 in suitable direction
+    switch(this.state.direction)
+    {
+        case'LEFT':
+        squares.push([head[0] - 4,head[1]]);
+        break;
+        case'RIGHT':
+        squares.push([head[0] + 4,head[1]]);
+        break;
+        case'UP':
+        squares.push([head[0],head[1] - 4]);
+        break;
+        case'DOWN':
+        squares.push([head[0],head[1] + 4]);
+        break;
+    }
+    squares.shift();// remove first element
+    this.setState({snakeSquares : squares});//update state
+  }
 
  
   render(){
